7432a9623b95547b9a42db01187bf4a62e8e07c3
Rafal <rgrajcar@gmail.com>
2023-03-16 11:49:31 +0100

    Dodaj pliki projektów.

---
+++ RafalGrajcar_zaj2.1/RafalGrajcar_zaj2.1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include "klasa.h"
+
+using namespace std;
+
+int main()
+{
+    
+    Klasa klasa;
+
+    Klasa klasa2(klasa);
+
+    Klasa klasa3 = klasa;
+
+    Klasa klasa4;
+    klasa4 = klasa;
+
+    Klasa* klasa5 = new Klasa;
+
+    cout << &klasa<<endl;
+    cout << &klasa2<<endl;
+    cout << &klasa3<<endl;
+    cout << &klasa4<<endl;
+    cout << &klasa5<<endl;
+
+    return 0;
+}
---
+++ RafalGrajcar_zaj2.1/klasa.cpp
@@ -0,0 +1,9 @@
+#include "klasa.h"
+
+Klasa::Klasa() {
+	cout << "Konstruktor"<<endl;
+}
+
+void wyswietl() {
+	cout << "Funkcja zaprzyjazniona" << endl;
+}
